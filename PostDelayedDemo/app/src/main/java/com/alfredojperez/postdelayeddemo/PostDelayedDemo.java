7a9176134c1eec72880c4e9b0f423408be9918f7
Lu Lin <lin_lu@columnbusstate.edu>
2019-05-22 00:09:17 -0400

    Initial commit

---
+++ PostDelayedDemo/app/src/main/java/com/alfredojperez/postdelayeddemo/PostDelayedDemo.java
@@ -0,0 +1,46 @@
+package com.alfredojperez.postdelayeddemo;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class PostDelayedDemo extends Activity implements Runnable{
+
+    private static final long PERIOD = 5000;
+    private View root = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_delayed_demo);
+        root = findViewById(android.R.id.content);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        run();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        //Remove any pending posts of Runnable r that are in the message queue.
+        root.removeCallbacks(this);
+    }
+
+    @Override
+    public void run() {
+        Toast.makeText(PostDelayedDemo.this, "Hello class: Yours Runnable", Toast.LENGTH_LONG).show();
+
+        // Causes the Runnable r to be added to the message queue, to be run after the specified
+        // amount of time elapses.
+        root.postDelayed(this, PERIOD);
+    }
+
+}
